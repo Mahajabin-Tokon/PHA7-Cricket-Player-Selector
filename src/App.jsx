5dd70bb0d9ca8b71e9e61546b70b4ac3592ce163
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-10-24 16:23:28 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Nav from "./components/Nav"
-import Banner from "./components/Banner"
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="w-11/12 mx-auto p-4 space-y-10">
-      <Nav/>
-      <Banner/>
-    </div>
+    <>
+      <div className="w-11/12 mx-auto p-4 space-y-10">
+        <Nav />
+        <Banner />
+      </div>
+      <Footer />
+    </>
   );
 };
 
